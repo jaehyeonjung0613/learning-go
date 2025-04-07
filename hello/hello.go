1ea54e52962a2464c0724a17ec70843f6acb5c2d
jaehyeonjung0613 <jaehyeonjung0613@gmail.com>
2025-04-07 15:14:31 +0900

    learn: Return greetings for multiple people - 이름 목록을 대상으로 인삿말  출력

--- hello/hello.go
+++ hello/hello.go
@@ -11,10 +11,12 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(log.Ldate)
 
-	message, err := greetings.Hello("Gladys")
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(message)
+	fmt.Println(messages)
 }
